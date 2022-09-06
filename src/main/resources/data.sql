363f8fba9e2f92a7faa9a470a2a03d5193e3cbe1
mohamedsoeb ahmednashirwala <miran@mohamedsoebs-MacBook-Pro.local>
2022-09-06 14:15:34 +0530

    second

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE countries (
+  ID INT AUTO_INCREMENT  PRIMARY KEY,
+  name VARCHAR(255) not null
+);
+
+INSERT INTO countries (id, name) VALUES (1, 'USA');
+INSERT INTO countries (id, name) VALUES (2, 'France');
+INSERT INTO countries (id, name) VALUES (3, 'Brazil');
+INSERT INTO countries (id, name) VALUES (4, 'Italy');
+INSERT INTO countries (id, name) VALUES (5, 'Canada');
